4b5b38e4c15b5d24d79dac7a0bf0dbdfb0680f15
Himasha Wijewickrama <himashawije5678@gmail.com>
2025-07-11 04:02:56 +0530

    Refactor ProductList component layout for improved structure and styling

--- src/productList.js
+++ src/productList.js
@@ -4,12 +4,17 @@ const ProductList = ({ products, setCart }) => {
   return (
     <div className="product-list">
       {products?.map((product) => (
-        <div key={product.id} className="product-item">
-          <h3>{product.name}</h3>
-          <p>Price: ${product.price}</p>
-          <button onClick={() => setCart((cart)=>[...cart,product])}>Add to Cart</button>
+          <div className="row-md-12" >
+            <div className="col-md-4" key={product.id}>
+              <h3>{product.name}</h3>
+              <button onClick={() => setCart((cart) => [...cart, product])}>
+                Add to Cart
+              </button>
+          </div>
         </div>
       ))}
     </div>
   );
-}
+};
+
+export default ProductList;
