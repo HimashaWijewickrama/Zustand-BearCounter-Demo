a3df5d1578756fd7feba1f05d166855d56117264
Himasha Wijewickrama <himashawije5678@gmail.com>
2025-07-11 03:30:52 +0530

    Add products data to the application

---
+++ src/products.js
@@ -0,0 +1,27 @@
+export const PRODUCTS = [
+    {
+        ID: 1,
+        NAME: "Product 1",
+        DESCRIPTION: "This is the description for Product 1.",
+    },
+    {
+        ID: 2,
+        NAME: "Product 2",
+        DESCRIPTION: "This is the description for Product 2.",
+    },
+    {
+        ID: 3,
+        NAME: "Product 3",
+        DESCRIPTION: "This is the description for Product 3.",
+    },
+    {
+        ID: 4,
+        NAME: "Product 4",
+        DESCRIPTION: "This is the description for Product 4.",
+    },
+    {
+        ID: 5,
+        NAME: "Product 5",
+        DESCRIPTION: "This is the description for Product 5.",
+    },
+]
